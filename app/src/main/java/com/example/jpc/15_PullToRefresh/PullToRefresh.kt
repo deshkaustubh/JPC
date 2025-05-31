ea6e111bc261ed0e404c7a7c1c6c9eaae51444bd
Kaustubh <ksd.tcet@gmail.com>
2025-05-31 20:27:57 +0530

    Initial Commit

--- app/src/main/java/com/example/jpc/15_PullToRefresh/PullToRefresh.kt
+++ app/src/main/java/com/example/jpc/15_PullToRefresh/PullToRefresh.kt
@@ -1,52 +1,115 @@
 package com.example.jpc.`15_PullToRefresh`
 
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.pullToRefreshIndicator
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
-fun PullToRefreshSample() {
+fun PulltoRefreshScreen() {
+
     val scope = rememberCoroutineScope()
     var isRefreshing by remember { mutableStateOf(false) }
     var items by remember { mutableStateOf(List(20) { "item $it" }) }
 
-    fun refreshItem() {
-
+    fun refreshItems() {
         scope.launch {
             isRefreshing = true
             delay(2000)
-            items = List(20) { "#${(0..100).random()} items selected at random" }
+            items = List(20) { "item #${(0..100).random()}" } // simulate data change
             isRefreshing = false
+        }
+    }
+
+    PulltoRefreshCustomIndicatorSample(
+        items = items,
+        isRefreshing = isRefreshing,
+        onRefresh = { refreshItems() }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PulltoRefreshCustomIndicatorSample(
+    items: List<String>,
+    isRefreshing: Boolean,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val state = rememberPullToRefreshState()
+
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = onRefresh,
+        state = state,
+        modifier = modifier.fillMaxSize(),
+        indicator = {
+            MyCustomIndicator(
+                state = state,
+                isRefreshing = isRefreshing
+            )
+        }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LazyColumn(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                items(items) {
+                    ListItem({ Text(text = it) })
+                }
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PullToRefreshCustomIndicatorSample(
-    items: List<String>,
+fun MyCustomIndicator(
+    state: PullToRefreshState,
     isRefreshing: Boolean,
-    onRefresh :Unit,
     modifier: Modifier = Modifier
-){
-
-    val state = rememberPullToRefreshState()
-
-    //PullToRefreshBox()
+) {
+    Box(
+        modifier = Modifier.pullToRefreshIndicator(
+            state = state,
+            isRefreshing = isRefreshing,
+            containerColor = PullToRefreshDefaults.containerColor,
+            threshold = PullToRefreshDefaults.PositionalThreshold
+        ),
+        contentAlignment = Alignment.Center
+    ) {
+        Crossfade(
+            targetState = isRefreshing,
+            animationSpec = tween(durationMillis = 1000),
+            modifier = Modifier.align(Alignment.Center)
+        ) { refreshing ->
+            if (refreshing) {
+                CircularProgressIndicator(Modifier.size(20.dp))
+            }
+        }
+    }
 }
-
 
 @Preview(showSystemUi = true)
 @Composable
 fun RefreshPreview() {
-
+    PulltoRefreshScreen()
 }
